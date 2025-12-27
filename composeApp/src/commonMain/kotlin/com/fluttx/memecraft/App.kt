29a7ed03afef93cf96f4860cad59aa90b0d24372
alamin-karno <amin15-1951@diu.edu.bd>
2025-12-28 03:03:57 +0600

    refactor: simplify App composable
    Removes unused imports and placeholder UI code from the `App` composable. The main content is now delegated to the `GalleryScreen`, cleaning up the primary entry point.

--- composeApp/src/commonMain/kotlin/com/fluttx/memecraft/App.kt
+++ composeApp/src/commonMain/kotlin/com/fluttx/memecraft/App.kt
@@ -1,25 +1,9 @@
 package com.fluttx.memecraft
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import com.fluttx.memecraft.core.theme.MemeCraftTheme
 import com.fluttx.memecraft.gallery.presentation.GalleryScreen
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import memecraft.composeapp.generated.resources.Res
-import memecraft.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
